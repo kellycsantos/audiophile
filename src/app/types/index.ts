e0d10432e783750c65a2160aa3d4419875627d91
Kelly C Santos <kellycristinasntos@gmail.com>
2026-06-02 21:35:51 -0300

    fix: removendo comentarios

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,37 +1,3 @@
-// type ImagesType = {
-//     desktop: string,
-//     mobile: string,
-//     tablet: string
-// }
-
-// type IncludesType = {
-//     quantity: number,
-//     item: string
-// }
-// type OthersType = {
-//     slug: string,
-//     name: string,
-//     image: ImagesType
-// }
-
-
-// export type ProductType = {
-//     id: number,
-//     slug: string,
-//     name: string,
-//     image: ImagesType,
-//     category: string,
-//     categoryImage: ImagesType,
-//     new: boolean,
-//     price: number,
-//     description: string,
-//     features: string,
-//     includes: IncludesType[],
-//     gallery: any, //todo: criar tipo para galeria
-//     others: OthersType[],
-// }
-
-
 export type ImagesType = {
   mobile: string;
   tablet: string;
