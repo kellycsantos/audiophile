089f78e9a8760a33f62b31c51a582305e46d3a4b
Kelly C Santos <kellycristinasntos@gmail.com>
2026-06-29 23:57:11 -0300

    feat: store criada

---
+++ src/app/store/index.ts
@@ -0,0 +1,57 @@
+import { create } from 'zustand';
+import { CartType, ProductType } from '../types';
+
+type State = CartType;
+
+type Action = {
+    addItem: (item: ProductType, quantity: number) => void
+    removeAllItems: () => void,
+    sumTotalAmount: () => void,
+}
+
+
+export const useCartStore = create<State & Action>((set, get) => ({
+    totalAmount: 0,
+    totalItems: 0,
+    items: [],
+
+    sumTotalAmount: () =>
+        set((state) => {
+            return { totalAmount: state.items.reduce((value, item) => value + (item.price * (item.quantity ?? 1)), 0) }
+        }),
+
+    addItem: (item: ProductType, quantity: number) => {
+
+        set((state) => {
+            const exists = state.items.find(
+                (product) => product.id === item.id
+            );
+
+            if (exists) {
+                return {
+                    items: state.items.map((product) =>
+                        product.id === item.id
+                            ? { ...product, quantity: (product.quantity ?? 1) + quantity }
+                            : product,
+                    ),
+                };
+            }
+
+            return {
+                items: [...state.items, { ...item, quantity: quantity }],
+
+            };
+        })
+        get().sumTotalAmount();
+    },
+
+
+
+    removeAllItems: () =>
+        set({
+            items: [],
+            totalAmount: 0,
+            totalItems: 0,
+        }),
+
+}))
